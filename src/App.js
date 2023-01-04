968c49c39e3fe13ffeaca3b54775aea73eb8bbde
Artrit Dili <artrit.dili@zeitfracht.de>
2023-01-04 13:38:55 +0100

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
-      <h1 className='text-2xl bg-red-500'>Hello World!</h1>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/offers" element={<Offers></Offers>}></Route>
+        <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
+        <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+        <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+      </Routes>
+     </Router>
     </>
   );
 }
